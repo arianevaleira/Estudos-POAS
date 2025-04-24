58400ca1d13e597145250e439245b39587b3664e
Ariane valeria <153088012+arianevaleira@users.noreply.github.com>
2025-04-24 15:19:29 -0300

    Create main.py

---
+++ aula1/main.py
@@ -0,0 +1,10 @@
+#Cria uma variavel, ou dicionario para guardar dados 
+# Biblioteca pudantic e flast api
+# Uvicorn (Servidor ASI)
+#Precisa da virtual env
+#pip install fastapi uvicorn pydantic 
+#pip freeze > requeriments.txt
+
+
+
+
