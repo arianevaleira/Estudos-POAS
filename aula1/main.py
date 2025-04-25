58fbc72f390d998394da6cac10623267f610b78c
Ariane valeria <153088012+arianevaleira@users.noreply.github.com>
2025-04-25 08:04:11 -0300

    Update main.py

--- aula1/main.py
+++ aula1/main.py
@@ -1,35 +1,23 @@
-#Cria uma variavel, ou dicionario para guardar dados 
-# Biblioteca pudantic e flast api
-# Uvicorn (Servidor ASI)
-#Precisa da virtual env
-#pip install fastapi uvicorn pydantic 
-#pip freeze > requeriments.txt
-#O pydantic com o fast api e o post é feito com uma estrutura  em json, as api convertem ele em um objeto 
-#uvicorn main:app ele não recarrega automaticamente
-#http://127.0.0.1:8000/docs 
-
 from fastapi import FastAPI, HTTPException
 from models import Tarefa
 from typing import List
 
 app = FastAPI()
 
-tarefas:List[Tarefa]=[] #Vai ser uma lista tipada, ou seja : -> Significa o tipo da variavel ou seja essa variavel so aceita o tipo tarefa
+tarefas: List[Tarefa] = []
 
-@app.get("/tarefas/", response_model=List[Tarefa]) #ele vai retorna uma lista de tarefas
+@app.get("/tarefas/", response_model=List[Tarefa])
 def listar_tarefas():
-    return tarefas #Retorna a variavel tarefas
+    return tarefas
 
-@app.get("/tarefas/ {id}", response_model=Tarefa)
-def listar_tarefas(id:int):
+@app.get("/tarefas/{id}", response_model=Tarefa)
+def listar_tarefa(id: int):
     for tarefa in tarefas:
         if tarefa.id == id:
             return tarefa
-    return HTTPException(status_code=404, detail="Não encrontrado")
+    raise HTTPException(status_code=404, detail="Não encontrado")
 
-@app.post("/tarefas/", response_model=Tarefa) #Cadastra tarefa
-def criar_tarefa(tarefa:Tarefa):#tem que ser compativel com um que voce passa
+@app.post("/tarefas/", response_model=Tarefa)
+def criar_tarefa(tarefa: Tarefa):
     tarefas.append(tarefa)
     return tarefa
-
-
