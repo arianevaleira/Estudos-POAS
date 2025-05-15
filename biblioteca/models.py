0f070b4b0632b822253dc1465349a05cf9afd17c
Ariane valeria <153088012+arianevaleira@users.noreply.github.com>
2025-05-15 08:07:41 -0300

    Update models.py

--- biblioteca/models.py
+++ biblioteca/models.py
@@ -1,8 +1,36 @@
 from pydantic import BaseModel
+from typing import List
+from datetime import date
+
+class Usuario(BaseModel):
+    id: int
+    username:str
+    password:str
+    data_criacao:date
+
+class Livro(BaseModel):
+    id: int
+    titulo:str
+    ano:int
+    edicao:int
+
+class LivroCreate(BaseModel):
+    titulo:str
+    ano:int
+    edicao:int
 
 
-class Tarefa(BaseModel):
-    id:int
-    descricao:str
-    prioridade:int
-    concluida:bool
+class Emprestimo(BaseModel):
+    id: int
+    usuario:Usuario
+    livro:Livro
+    data_emprestimo:date
+    data_devolucao:date
+
+class Biblioteca(BaseModel):
+    id: int
+    nome:str
+    acervo: List["Livro"]
+    usuarios: List["Usuario"]
+    emprestimos: List["Emprestimo"]
+    
