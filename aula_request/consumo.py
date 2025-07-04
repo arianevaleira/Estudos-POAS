cb934137dd08a6a80d2a8be9a6e0170f8c629da1
arianevaleira <costa.valeria@escolar.ifrn.edu.br>
2025-07-04 15:14:32 -0300

    Atividade Extra

--- aula_request/consumo.py
+++ aula_request/consumo.py
@@ -1,38 +1,66 @@
 import requests
+URL = 'http://127.0.0.1:8000'
 
-URL = 'http://127.0.0.1:8000/'
-def listar_livros(titulo):
-   r = requests.get(f'{URL}/livros') 
-   if r.status_code == 200:
-       print(r.text)
+def listar_livros():
+    r = requests.get(f"{URL}/livros")
+    if r.status_code == 200:
+        print(r.text)
+    elif r.status_code == 404:
+        print(r.text)
 
+def listar_livro(titulo):
+    r = requests.get(f"{URL}/livros/{titulo}")
+    if r.status_code == 200:
+        print(r.text)
+    elif r.status_code == 404:
+        print(r.text)
 
 def cadastrar_livros():
-    pass
+    titulo = input('Digite o título: ')
+    ano = input('Digite o ano: ')
+    edicao = input('Digite a edição: ')
 
-def excluir_livros():
-    pass
+    livro = {
+        'titulo' : titulo,
+        'ano' : ano,
+        'edicao' : edicao
+    }
+
+    r = requests.post(f'{URL}/livros', json=livro)
+
+def excluir_livro(titulo):
+    r = requests.delete(f'{URL}/livros/{titulo}')
+    if r.status_code == 200:
+        print('Excluído com sucesso')
+    else:
+        print(r.text)
 
 def menu():
-    print("1 - Listar Livros")
-    print('2 - Listar Livros Pelo titulo')
-    print('3 - Cadastrar Livros')
-    print('4 - Deletar Livros')
+    print('1 - Listar Livros')
+    print('2 - Listar Livros pelo título')
+    print('3 - Cadastrar Livro')
+    print('4 - Deletar Livro')
     print('5 - Sair')
-    return int(input('Digite sua escolha: '))
+    return int(input('Digite sua opção: '))
 
 opcao = menu()
-
 
 while opcao != 5:
     if opcao == 1:
         listar_livros()
-    elif opcao == 2:
-        titulo = input('Digite  titulo: ')
-        listar_livros(titulo)
-    elif opcao == 3:
-       cadastrar_livros()
-    elif opcao == 4:
-        titulo = input("Digit o nome do livro: ")
-        excluir_livros(titulo)
 
+    elif opcao == 2:
+        titulo = input('Digite o título: ')
+        listar_livro(titulo)
+
+    elif opcao == 3:
+        cadastrar_livros()
+
+    if opcao == 4:
+        titulo = input('Digite o título: ')
+        excluir_livro(titulo)
+
+    elif opcao == 5:
+        break
+
+    opcao = menu()
